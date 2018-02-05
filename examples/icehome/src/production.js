0795217a37f058a4ea23519498c5b12657bf5e61
axetroy <troy450409405@gmail.com>
2018-02-05 09:57:09 +0800

    refactor: Update desc

--- examples/icehome/src/production.js
+++ examples/icehome/src/production.js
@@ -7,20 +7,17 @@ const productions = [
   },
   {
     name: '西洋参阿胶膏',
-    desc:
-      '阿胶糕福牌阿胶作为主料，以核桃、红枣、枸杞、冰糖作为辅料，精心熬制而成',
+    desc: '',
     image: '/static/xi_yang_shen_e_jiao_gao.jpg'
   },
   {
     name: '枣夹核桃',
-    desc:
-      '阿胶糕福牌阿胶作为主料，以核桃、红枣、枸杞、冰糖作为辅料，精心熬制而成',
+    desc: '精心选自新疆和田大枣以及新疆吐鲁番葡萄干和精选核桃仁手工制作而成。',
     image: '/static/zao_jia_he_tao.jpg'
   },
   {
     name: '祛湿茶',
-    desc:
-      '阿胶糕福牌阿胶作为主料，以核桃、红枣、枸杞、冰糖作为辅料，精心熬制而成',
+    desc: '28味药材调制而精，传承秘方，适用于湿寒/热体质',
     image: '/static/qu_si_cha.jpg'
   }
 ];
